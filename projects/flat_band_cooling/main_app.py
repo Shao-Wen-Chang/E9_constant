ca8b2d914f0dbf7748b0ddabea049f41c1a09ea8
swchang12 <swchang12@berkeley.edu>
2024-01-19 08:26:26 -0800

    added some basic logging infrastructure

--- projects/flat_band_cooling/main_app.py
+++ projects/flat_band_cooling/main_app.py
@@ -1,10 +1,28 @@
+import logging
+import time
+
 import simple_fermi_bose as sfb
 import fermi_with_reservoir as fwr
 
 def main():
+    # User inputs
+    logpath = '' # '' if not logging to a file
+    loglevel = logging.INFO
     module = sfb
-    kwargs = {"calculation_mode": "isentropic"}
+    kwargs = {"calculation_mode": "isentropic"
+              }
+    
+    # Configuring logger (reset and reconfigured at each run)
+    start_time = time.time()
+    logroot = logging.getLogger()
+    list(map(logroot.removeHandler, logroot.handlers))
+    list(map(logroot.removeFilter, logroot.filters))
+    logging.basicConfig(filename = logpath, level = loglevel)
+    logging.info("main started")
+
+    # Run module main()
     module.main(**kwargs)
+    logging.info("main exited successfully, time = {:.2f}s".format(time.time() - start_time))
 
 if __name__ == "__main__":
     main()
